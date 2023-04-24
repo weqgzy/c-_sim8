671c30bffc5dc8e212440a5b7db00500ddcfb6ff
SAVAGE <proj_1>
2023-04-24 09:47:00 +0300

    all_done

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿int [,] arr = new int [3,4];
+GetArray(arr);
+PrintArray(arr);
+  
+  
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1) - 1; j++)
+        {
+            for (int z = 0; z < arr.GetLength(1) - 1; z++)
+            {
+                if(arr[i,z] < arr[i , z + 1]){
+                    int temp = 0;
+                    temp = arr[i , z];
+                    arr[i,z] = arr[i , z + 1];
+                    arr[i , z + 1] = temp;              
+                }
+            }
+        }
+    }
+
+
+Console.WriteLine();
+Console.WriteLine("Массив с упорядоченными значениями: ");
+PrintArray(arr);
+
+void GetArray (int [,] array)
+{
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array [i,j] = new Random().Next(1,9);
+    }
+    System.Console.WriteLine("");
+}
+}
+
+void PrintArray(int [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write(array[i , j] + " ");
+        }
+        System.Console.WriteLine("");
+    }
+}
+
+
+
+
+
+
